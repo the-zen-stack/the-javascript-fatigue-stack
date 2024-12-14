4a85fd30801ebeb0ceb342ea50c41ef742018c6f
matangubkin <Matangub@gmail.com>
2024-12-14 22:43:07 +0200

    fix

--- src/components/header.ts
+++ src/components/header.ts
@@ -6,8 +6,7 @@ export const Header = (): string => /*html*/ `
     <div class="nav-content">
         <a href="/" class="nav-brand">${APP_TITLE}</a>
         <div class="nav-links">
-            <a href="/">Home</a>
-            <a href="/posts">Posts</a>
+            <a href="/">Posts</a>
             <a href="/posts/new-post">New Post</a>
         </div>
         ${ThemeToggle()}
